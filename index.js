957ef946a3c4559e4650bd1942b75266cd5c22f8
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-28 22:05:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num2=2;
+const num1=31;
+const multiply = num1 * num2;
 
+const random=Math.ceil(Math.random() * 10)+1;
+
+const mod = 16%6;
+
+const max = Math.max(1,2,3,20);
 // Write your code here
